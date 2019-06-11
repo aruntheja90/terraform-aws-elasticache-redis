fc066d11e9c32f106846dec3eaef4ca4fb34b7cf
kristiantokarim <kristiantokarim@gmail.com>
2019-06-11 16:00:56 +0700

    [TXT] create redis module

---
+++ README.md
@@ -0,0 +1,7 @@
+# terraform-aws-elasticache-redis
+terraform module to create elasticache redis
+
+License
+-------
+
+Apache 2 Licensed. See LICENSE for full details.
---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "cluster_id" {
+  description = "The cluster identifier of this memcached"
+  value       = "${aws_elasticache_cluster.this.cluster_id}"
+}
+
+output "az_mode" {
+  description = "The provisioning mode of this memcached"
+  value       = "${aws_elasticache_cluster.this.az_mode}"
+}
+
+output "security_group_ids" {
+  description = "The security groups attached to this memcached"
+  value       = "${aws_elasticache_cluster.this.security_group_ids}"
+}
+
+output "parameter_group_name" {
+  description = "The parameter group used by this memcached"
+  value       = "${aws_elasticache_cluster.this.parameter_group_name}"
+}
+
+output "port" {
+  description = "Port used for this memcached"
+  value       = "${aws_elasticache_cluster.this.port}"
+}
+
+output "subnet_group_name" {
+  description = "The subnet group name where this memcached resides in"
+  value       = "${aws_elasticache_cluster.this.subnet_group_name}"
+}
